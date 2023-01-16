c3c025684473772f5dab7cc371ec9f462f33efbc
Jupiteriana <xiong0201y@163.com>
2023-01-16 19:39:55 +0800

    fix homepage

--- src/pages/home/components/LinearGradientText.jsx
+++ src/pages/home/components/LinearGradientText.jsx
@@ -1,7 +1,29 @@
-function LinearGradientText(props) {
-  const { text } = props;
+import { Typography } from '@douyinfe/semi-ui';
+import cns from 'classnames';
+import styled from 'styled-components';
+import sphere from '@/assets/imgs/sphere.png';
 
-  return <div>{text}</div>;
+const GradientText = styled(Typography.Text)`
+  background: linear-gradient(267.96deg, #7A43FF 5.78%, #00FFD1 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  color: transparent;
+`;
+
+function LinearGradientText(props) {
+  const {
+    text, className, textClassName, showImg,
+  } = props;
+
+  return (
+    <div className={cns('inline-block relative', className)}>
+      { showImg && <img width="98" src={sphere} alt="sphere" />}
+      <GradientText className={cns(textClassName)}>
+        {text}
+      </GradientText>
+    </div>
+  );
 }
 
 export default LinearGradientText;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,19 @@
+import { Typography } from '@douyinfe/semi-ui';
+import LinearGradientText from './components/LinearGradientText';
+
 function Home() {
-  return <div>home page</div>;
+  return (
+    <div className="mt-16">
+      <div className="flex items-center">
+        <div className="w-3/5">
+          <LinearGradientText text={<>All Work <br />  On-Chain.</>} />
+          <Typography.Text>A permissionless protocol to facilitate the exchange of MarkerDAO work, knowledge, and contribution.</Typography.Text>
+          <button>
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
