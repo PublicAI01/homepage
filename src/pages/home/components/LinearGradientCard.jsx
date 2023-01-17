ce32d94973f92d276c76671b33cff6e6557040d5
Evan810828 <1015021084@qq.com>
2023-01-17 13:39:15 +0800

    linear gradient card

--- src/pages/home/components/LinearGradientCard.jsx
+++ src/pages/home/components/LinearGradientCard.jsx
@@ -9,21 +9,25 @@ export function LinearGradientCard(params) {
   const CardWrapper = styled.div`
     color: white;
     box-sizing: border-box;
-    padding: 3px;
+    padding: 2px;
     background-image: ${border};
     border-radius: 8px;
   `;
 
   const SplitLine = styled.div`
-    border: 1px solid;
-    border-image: ${border} 2;
+    background-image: ${border};
   `;
 
   return (
     <CardWrapper className={cns(className)}>
       <div className="rounded-[8px] w-full h-full bg-black p-3 text-center">
-        <div>{title}</div>
-        <SplitLine className="w-[50px] h-[1px] relative left-1/2 translate-x-right-1/2" />
+        <div className="h-1/2">
+          <div className="w-full h-full">
+            <img className="w-full h-full" src={icon} alt="" />
+            <div className="relative bottom-2/3 text-[24px]">{title}</div>
+          </div>
+        </div>
+        <SplitLine className="w-[150px] h-[4px] relative left-1/2 translate-x-right-1/2 my-4" />
         <div>{text}</div>
       </div>
     </CardWrapper>
