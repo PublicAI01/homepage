f2e485d8cb6d5921d665e0e1d15bbd61f6cad0f9
Jupiteriana <xiong0201y@163.com>
2023-02-02 11:17:14 +0800

    improve ui

--- src/pages/home/components/section/section1.jsx
+++ src/pages/home/components/section/section1.jsx
@@ -1,5 +1,5 @@
 import { Typography } from '@douyinfe/semi-ui';
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { DEVICE } from '@/config/device';
 import LinearGradientText from '../LinearGradientText';
@@ -14,9 +14,9 @@ const StyledSplineBox = styled.div`
   right: 0;
   height: 600px;
 
-  >canvas{
+  /* >canvas{
     transform: scale(1.2);
-  }
+  } */
 
   @media ${DEVICE.xmd} {
     height: 500px;
@@ -25,7 +25,7 @@ const StyledSplineBox = styled.div`
     width: 100%;
     top: 100px;
     z-index: -10;
-    transform: scale(1.2);
+    /* transform: scale(1.2); */
   }
 
 `;
@@ -33,16 +33,28 @@ const StyledSplineBox = styled.div`
 function Section1() {
   const splineRef = useRef();
   const splineApp = useRef();
+  const splineBoxRef = useRef();
+
+  // useEffect(() => {
+  //   if (splineBoxRef.current) {
+  //     document.body.addEventListener('scroll', () => {
+  //       console.log('body scroll');
+  //     });
+  //     splineBoxRef.current.addEventListener('scroll', () => {
+  //       console.log('box scroll');
+  //     });
+  //   }
+  // }, [splineBoxRef]);
 
   const onLoad = (_splineApp) => {
     splineApp.current = _splineApp;
-    _splineApp.setZoom(1.2);
+    _splineApp.setZoom(0.65);
   };
 
   return (
     <SectionWrap className="flex items-center min-h-[calc(100vh_-_120px)] xmd:!pt-0" id="home">
       <div className="nmd:w-3/5 relative z-2 xmd:bg-black/40 p-2 xmd:py-24">
-        <div className="relative z-10">
+        <div className="relative z-10 pointer-events-none">
           <div>
             <LinearGradientText
               textClassName="text-[128px] leading-none xmd:text-[48px]"
@@ -55,7 +67,7 @@ function Section1() {
           Get Started
         </button>
       </div>
-      <StyledSplineBox>
+      <StyledSplineBox ref={splineBoxRef}>
         <Suspense fallback={<div>Loading...</div>}>
           <Spline ref={splineRef} onLoad={onLoad} scene="https://prod.spline.design/RWc0Ma8j0CUcDh0e/scene.splinecode" />
         </Suspense>
