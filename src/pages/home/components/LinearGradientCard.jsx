45c71c039797328ddf822ff816e9e3a565b77e4f
Jupiteriana <xiong0201y@163.com>
2023-02-06 12:44:55 +0800

    fix improve ui

--- src/pages/home/components/LinearGradientCard.jsx
+++ src/pages/home/components/LinearGradientCard.jsx
@@ -22,11 +22,9 @@ export function LinearGradientCard(params) {
   return (
     <CardWrapper className={cns(className)}>
       <div className="rounded-[8px] w-full h-full bg-black p-3 text-center">
-        <div className="h-1/2">
-          <div className="w-full h-full">
-            <img className="w-full h-full" src={icon} alt="" />
-            <Typography.Text className="relative bottom-2/3 text-[24px] font-bold">{title}</Typography.Text>
-          </div>
+        <div className="relative">
+          <img className="mx-auto w-[240px] h-[240px] object-contain" src={icon} alt="" />
+          <Typography.Text className="absolute w-full left-0 top-1/2 text-2xl font-bold xmd:static nmd:-translate-y-1/2">{title}</Typography.Text>
         </div>
         <div className="text-center mb-2">
           <SplitLine className="h-[4px] inline-block" />
