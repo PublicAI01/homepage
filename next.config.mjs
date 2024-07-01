2a1546c1de2aa760b278d0ef43bb46d3dfe6f2a5
moxiaov587 <zouj1awang@163.com>
2024-07-01 16:18:58 +0800

    🧱 ++

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find((rule) =>
+			rule.test?.test?.('.svg'),
+		);
+
+		config.module.rules.push(
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: /react/, // *.svg?react
+				use: ['@svgr/webpack'],
+			},
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /react/] },
+			},
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#4000C8',
+				b1: '#020202',
+				b2: '#0E100C',
+				b3: '#0f0f0f',
+				g1: '#e6e6e6',
+			},
+			animation: {
+				'card-flicker': 'card-flicker calc(var(--duration)*1s) 2 linear',
+			},
+			keyframes: {
+				'card-flicker': {
+					'0%': {
+						'background-color': '#000',
+					},
+					'33%': {
+						'background-color': 'rgba(255, 255, 255, 0.5)',
+					},
+					'100%': {
+						'background-color': '#000',
+					},
+				},
+			},
+		},
+	},
+	plugins: [
+		require('tailwind-scrollbar')({
+			nocompatible: true,
+			preferredStrategy: 'pseudoelements',
+		}),
+	],
 };
 export default config;
