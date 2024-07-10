7bec8ef8e3b82f88c913bc8659cef3acb953f278
moxiaov587 <zouj1awang@163.com>
2024-07-10 17:27:33 +0800

    🚸 Compatibility for iOS

--- src/app/components/Swiper/index.tsx
+++ src/app/components/Swiper/index.tsx
@@ -1,9 +1,42 @@
+'use client';
+
+import 'overlayscrollbars/overlayscrollbars.css';
+
+import { useOverlayScrollbars } from 'overlayscrollbars-react';
+import { useEffect, useRef } from 'react';
+
+import styles from '@/app/components/Swiper/Swiper.module.css';
+import { cn } from '@/utils';
+
 const Swiper = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [initialize] = useOverlayScrollbars({
+    options: {
+      showNativeOverlaidScrollbars: false,
+      overflow: {
+        x: 'hidden',
+        y: 'scroll',
+      },
+      scrollbars: {
+        theme: styles['os-theme'],
+        visibility: 'visible',
+      },
+    },
+    defer: true,
+  });
+
+  useEffect(() => {
+    if (containerRef.current) {
+      initialize(containerRef.current);
+    }
+  }, [initialize]);
+
   return (
     <div className="flex items-center md:w-2/3">
       <div
+        ref={containerRef}
         dir="rtl"
-        className="relative h-36 snap-y snap-mandatory overflow-y-scroll scrollbar scrollbar-track-white/10 scrollbar-thumb-primary scrollbar-track-rounded-full scrollbar-thumb-rounded-full scrollbar-w-1">
+        className={cn('relative h-36', styles.container)}>
         {[
           {
             title: 'Data Collection',
@@ -25,10 +58,10 @@ const Swiper = () => {
             dir="auto"
             key={index}
             className="ml-6 flex h-36 snap-center snap-always flex-col justify-between py-2">
-            <h5 className="text-xl font-bold text-white transition-colors md:text-4xl">
+            <h5 className="text-xl font-bold text-white transition-colors md:text-3xl lg:text-4xl">
               {item.title}
             </h5>
-            <p className="text-sm font-medium text-white transition-colors md:text-lg">
+            <p className="text-sm font-medium text-white transition-colors md:text-base lg:text-lg">
               {item.content}
             </p>
           </div>
