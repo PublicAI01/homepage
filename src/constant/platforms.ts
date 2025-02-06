f99e557c8b7f50029f800394aae6f4a5209b9df6
moxiaov587 <zouj1awang@163.com>
2025-02-06 10:41:20 +0800

    🚑️ Hiden x link

--- src/constant/platforms.ts
+++ src/constant/platforms.ts
@@ -1,16 +1,10 @@
 import discord from '@/assets/media-platform/discord.svg?react';
 import medium from '@/assets/media-platform/medium.svg?react';
 import telegram from '@/assets/media-platform/telegram.svg?react';
-import twitter from '@/assets/media-platform/twitter.svg?react';
-import {
-  DISCORD_LINK,
-  MEDIUM_LINK,
-  TELEGRAM_LINK,
-  TWITTER_LINK,
-} from '@/constant';
+import { DISCORD_LINK, MEDIUM_LINK, TELEGRAM_LINK } from '@/constant';
 
 const PLATFORMS = [
-  { Icon: twitter, label: 'x', link: TWITTER_LINK },
+  // { Icon: twitter, label: 'x', link: TWITTER_LINK },
   { Icon: telegram, label: 'telegram', link: TELEGRAM_LINK },
   { Icon: medium, label: 'medium', link: MEDIUM_LINK },
   { Icon: discord, label: 'discord', link: DISCORD_LINK },
