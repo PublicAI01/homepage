9cd7786e9f71ed404f9b8f0365e35b9f6c70c111
moxiaov587 <zouj1awang@163.com>
2025-06-18 08:18:45 +0800

    🐛 Missing parameter passing

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,6 +16,7 @@ const Button = (props: ButtonProps) => {
         'app-shadow w-60 py-2 text-center text-base font-medium text-white shadow-white md:text-xl',
         theme === 'primary' && 'bg-primary',
         theme === 'normal' && 'border-primary border-2 bg-white text-black',
+        className,
       )}
       style={{
         ...(!['primary', 'normal'].includes(theme)
