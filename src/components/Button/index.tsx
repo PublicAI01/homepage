e90576e2b4fcf000bd80b1f5a5ae3c7f5c97e0c1
moxiaov587 <zouj1awang@163.com>
2025-07-11 12:13:15 +0800

    💄 Update primary button background color

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,7 +16,7 @@ const Button = (props: ButtonProps) => {
         'app-shadow relative w-60 py-2 text-center text-base font-medium text-white shadow-white md:text-xl',
         'after:absolute after:inset-0.5 after:-z-1 after:overflow-hidden after:rounded-sm after:bg-linear-90 hover:after:from-[#5708FE] hover:after:to-[#999]',
         'active:translate-y-0.5 not-disabled:active:opacity-75 not-disabled:active:after:opacity-75 has-active:after:translate-y-0.5',
-        theme === 'primary' && 'bg-primary hover:bg-[#3700F0]',
+        theme === 'primary' && 'bg-[#4808FE] hover:bg-[#3700F0]',
         theme === 'normal' && 'border-primary border-2 bg-white text-black',
         className,
       )}
